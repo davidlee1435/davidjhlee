216025bd48116c58819f6f6344de4203187a4a2b
David Lee <jl4397@columbia.edu>
2017-02-21 20:54:29 -0800

    Reactify

--- static/js/init.js
+++
@@ -1,6 +0,0 @@
-var clipboard = new Clipboard('.email-btn');
-
-clipboard.on('success', function(e){
-  Materialize.toast('My email has been copied to your clipboard!', 6000);
-  e.clearSelection();
-});
